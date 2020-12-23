c31b08c62ca80aeffe0bccb37dfcf146c54d316e
bajankristof <bajankristof@gmail.com>
2020-12-23 11:57:17 +0100

    fix: char list formatting

--- src/lager_logstash.erl
+++ src/lager_logstash.erl
@@ -1,6 +1,6 @@
 -module(lager_logstash).
 
--export([format/1]).
+-export([format/1, jsonify/1]).
 
 -spec format(term()) -> atom() | binary() | list() | map().
 format(Pid) when erlang:is_pid(Pid) ->
@@ -14,12 +14,21 @@ format(Fun) when erlang:is_function(Fun) ->
 format(Tuple) when erlang:is_tuple(Tuple) ->
     erlang:list_to_binary(io_lib:format("~p", [Tuple]));
 format(List) when erlang:is_list(List) ->
-    case catch maps:from_list(List) of
-        #{} = Map -> format(Map);
-        _ -> lists:map(fun format/1, List)
+    case io_lib:char_list(List) of
+        true -> erlang:list_to_binary(List);
+        false -> jsonify(List)
     end;
 format(Map) when erlang:is_map(Map) ->
+    jsonify(Map);
+format(Safe) -> Safe.
+
+-spec jsonify(map() | list()) -> map() | list().
+jsonify(List) when erlang:is_list(List) ->
+    case catch maps:from_list(List) of
+        #{} = Map -> jsonify(Map);
+        _ -> lists:map(fun format/1, List)
+    end;
+jsonify(Map) when erlang:is_map(Map) ->
     maps:fold(fun (Key, Value, Acc) ->
         Acc#{format(Key) => format(Value)}
-    end, #{}, Map);
-format(Safe) -> Safe.
+    end, #{}, Map).
